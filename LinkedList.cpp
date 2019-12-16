c2b1470756c799b8f5ef8b9c0faba57385e18f79
GarrisonTravis <58957083+GarrisonTravis@users.noreply.github.com>
2019-12-16 17:37:22 -0600

    Updated LinkedList.cpp
    Added print and insertFront functions

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -21,7 +21,7 @@ LinkedList<Type>::LinkedList(Type val) {
 //Constructor with an array as a parameter
 //Post: Linked List will contain the contents of the array in the same order
 template <class Type>
-LinkedList<Type>::LinkedList(Type arr[]) {
+LinkedList<Type>::LinkedList(Type arr[], int size) {
 
 	Node<Type>* newNode = new Node<Type>;
 	head = newNode;
@@ -29,11 +29,38 @@ LinkedList<Type>::LinkedList(Type arr[]) {
 
 	Node<Type>* current = head;
 
-	for (int i = 0; i < 5; i++) {
+	for (int i = 0; i < size; i++) {
 		current->val = arr[i];
 
-		newNode = new Node<Type>;
-		current->next = newNode;
+		if (i != size - 1) {
+			newNode = new Node<Type>;
+			current->next = newNode;
+			current = current->next;
+		}
+		else
+			tail = current;
+	}
+}
+
+//Inserts a node at the front of the linked list
+//Post: A new node with the parameter will be inserted at the front of the linked list
+template <class Type>
+void LinkedList<Type>::insertFront(Type val) {
+	Node<Type>* temp = head;
+
+	head = new Node<Type>;
+	head->val = val;
+	head->next = temp;
+}
+
+//Prints the linked list
+template <class Type>
+void LinkedList<Type>::print() {
+	Node<Type>* current = head;
+
+	while (current) {
+		cout << current->val << " ";
 		current = current->next;
 	}
+	cout << endl;
 }
